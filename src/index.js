6884597686cec26d1a59bd93066ff756e3ce19d1
I A <ilie.adrian1986@gmail.com>
2024-03-06 20:39:55 +0200

    finished project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import { createHome } from './home.js';
 import { createMenu } from './menu.js';
 import { createAbout } from './about.js';
 
-// createHome();
-// createMenu();
-createAbout()
+const homeBtn = document.getElementById('home');
+const menuBtn = document.getElementById('menu');
+const aboutBtn = document.getElementById('about');
+const container = document.getElementById('content');
+
+homeBtn.addEventListener('click', function() {
+    container.innerHTML= '';
+    createHome();
+})
+
+menuBtn.addEventListener('click', function() {
+    container.innerHTML= '';
+    createMenu();
+});
+
+aboutBtn.addEventListener('click', function() {
+    container.innerHTML= '';
+    createAbout();
+});
+
+createHome();
